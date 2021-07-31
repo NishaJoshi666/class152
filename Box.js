ffbcb69faddd4657594effdabca6fbff77f0551f
Nisha Joshi <69041055+NishaJoshi666@users.noreply.github.com>
2021-07-31 20:41:47 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,44 @@
+// Registering component in box-component.js
+AFRAME.registerComponent("move-box", {
+  schema: {
+    moveX: { type: "number", default: 1 },
+  },
+
+  tick: function () {
+    window.addEventListener('click',(e)=>{
+    this.data.moveX = this.data.moveX + 0.01;
+    })
+
+    var pos = this.el.getAttribute("position");
+
+    pos.x = this.data.moveX;
+
+    this.el.setAttribute("position", {x: pos.x, y: pos.y, z: pos.z});
+  }
+});
+
+AFRAME.registerComponent("camera-zoom-out", {
+  schema: {
+    moveZ: {type: 'number', default: 10}
+  },
+  tick: function(){
+    this.data.moveZ = this.data.moveZ+0.01;
+    var pos = this.el.getAttribute("position");
+    pos.z = this.data.moveZ;
+    this.el.setAttribute("position", {x: pos.x, y: pos.y, z: pos.z})
+  }
+});
+
+AFRAME.registerComponent("fall-down", {
+  schema: {
+    moveY: {type: 'number', default: 0}
+  },
+  tick: function(){
+    window.addEventListener('click',(e)=>{
+    this.data.moveY = this.data.moveY+0.01;
+    })
+    var pos = this.el.getAttribute("position");
+    pos.y =pos.y+this.data.moveY;
+    this.el.setAttribute("position", {x: pos.x, y: pos.y, z: pos.z})
+  }
+});
